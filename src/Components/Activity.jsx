c6ca25aa1ca2176779a23f20bc11ae4b6dfa30ee
dgunadharma20 <dgunadharma20@gmail.com>
2021-11-24 00:36:22 -0800

    trying out different styles for activities page

--- src/Components/Activity.jsx
+++ src/Components/Activity.jsx
@@ -5,18 +5,16 @@ function Activity(props) {
     const activity = props.activity;
     return (
         <div>
-            <a href="/location" className="activityLink">
-                <Card className="activityCard d-flex mx-auto my-auto" style={{ width: '22rem' }}>
-                    <Card.Header variant="top">
-                        <Card.Img className="activityImg mt-2 mb-2" variant="top" src="https://picsum.photos/2000" />
-                    </Card.Header>
-                    <Card.Body className="activityText">
-                        <Card.Title className="text-dark">{activity.name}</Card.Title>
-                        <Card.Subtitle className="text-dark">{activity.location}</Card.Subtitle>
-                        <Button className="mt-2" variant="outline-dark">Check Out Locations!</Button>
-                    </Card.Body>
-                </Card>
-            </a>
+            <Card border="light" className="activityCard d-flex mx-auto my-auto text-center" style={{ width: '22rem' }}>
+                <Card.Header variant="top">
+                    <Card.Img className="activityImg mt-2 mb-2" variant="top" src="https://picsum.photos/2000" />
+                </Card.Header>
+                <Card.Body className="activityText justify-content-center">
+                    <Card.Title>{activity.name}</Card.Title>
+                    <Card.Subtitle>{activity.seasons}</Card.Subtitle>
+                    <Button className="mt-2 text-center" variant="outline-dark">Check Out Locations</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
