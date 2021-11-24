64f9733c7a37c7b9cbbf18ac009054b7e7d2ebaa
avi-anviam <avirajdhooria2001@gmail.com>
2021-11-24 10:06:25 -0800

    worked on activity.jsxand activities.json

--- src/Components/Activity.jsx
+++ src/Components/Activity.jsx
@@ -2,17 +2,18 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 function Activity(props) {
-    const activity = props.activity;
+    const activity = props.activity
     return (
         <div>
             <Card border="light" className="activityCard d-flex mx-auto my-auto text-center" style={{ width: '22rem' }}>
                 <Card.Header variant="top">
-                    <Card.Img className="activityImg mt-2 mb-2" variant="top" src="https://picsum.photos/2000" />
+                    <Card.Img className="activityImg mt-2 mb-2" variant="top" src={activity.img} />
                 </Card.Header>
                 <Card.Body className="activityText justify-content-center">
                     <Card.Title>{activity.name}</Card.Title>
                     <Card.Subtitle>{activity.seasons}</Card.Subtitle>
-                    <Button className="mt-2 text-center" variant="outline-dark">Check Out Locations</Button>
+                    <Card.Text>{activity.description}</Card.Text>
+                    <Button className="mt-2 text-center" variant="outline-dark">Locations</Button>
                 </Card.Body>
             </Card>
         </div>
