c7e8adf02085709bbbe681f13a15cbac0f94ecca
dgunadharma20 <dgunadharma20@gmail.com>
2021-11-23 16:19:51 -0800

    minor changes to home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,10 +12,10 @@ function Home() {
                     <img
                         className="d-block w-100"
                         src="../images/ski.jpeg"
-                        alt=""
+                        alt="skiing"
                     />
-                    <Carousel.Caption style={{ bottom: "18%", letterSpacing: "10pt" }}>
-                        <div className="animated fadeInDown">
+                    <Carousel.Caption style={{ bottom: "22%", letterSpacing: "10pt" }}>
+                        <div>
                             <h1 className="carouselTitle">SKIING</h1>
                         </div>
                     </Carousel.Caption>
@@ -24,10 +24,10 @@ function Home() {
                     <img
                         className="d-block w-100"
                         src="../images/hike.jpeg"
-                        alt=""
+                        alt="hiking"
                     />
-                    <Carousel.Caption style={{ bottom: "18%", letterSpacing: "10pt" }}>
-                        <div className="animated fadeInDown">
+                    <Carousel.Caption style={{ bottom: "22%", letterSpacing: "10pt" }}>
+                        <div>
                             <h1 className="carouselTitle">HIKING</h1>
                         </div>
                     </Carousel.Caption>
@@ -36,10 +36,10 @@ function Home() {
                     <img
                         className="d-block w-100"
                         src="../images/bike.jpeg"
-                        alt=""
+                        alt="biking"
                     />
-                    <Carousel.Caption style={{ bottom: "18%", letterSpacing: "10pt" }}>
-                        <div className="animated fadeInDown">
+                    <Carousel.Caption style={{ bottom: "22%", letterSpacing: "10pt" }}>
+                        <div>
                             <h1 className="carouselTitle">BIKING</h1>
                         </div>
                     </Carousel.Caption>
@@ -49,11 +49,22 @@ function Home() {
                 <Row>
                     <Col className="welcomeMessage">
                         <h2>Welcome to MountainStop!</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
-                        in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>
+                            Mountain sports are well-loved but trip planning can be stressful due to treacherous weather, inadequate resort information, 
+                            planning rentals and more. Skiing, snowboarding and hiking for example are highly dependent on safe weather and good conditions 
+                            while also being very gear- intensive. When it comes to trip planning, weather information across multiple locations is imperative. 
+                            In order to obtain sufficient information on weather and mountain conditions, individuals currently need to go through numerous 
+                            applications in order to find sufficient information for multiple locations. Furthermore, trip planning also includes elements 
+                            such as rentals, resort information, drive times and availability. There are many existing sites that show weather and some that 
+                            show trips but no sites cater to trip planning by including all relevant information on one site while also showing similar mountains 
+                            or other trip options.
+                        </p>
+                        <p>
+                            Our web application’s aim is to be a central hub for students, tourists and families to find information before they plan their next outdoor
+                            adventure. With this web application, individuals will be able to find the best rentals, weather and mountain conditions, as well as leave 
+                            reviews and “share” trips with others. This addresses the critical need for comprehensive trip planning based on factual metrics and real-time 
+                            data which will reduce user frustration and increase outdoor sports satisfaction.
+                        </p>
                     </Col>
                 </Row>
                 <Row>
