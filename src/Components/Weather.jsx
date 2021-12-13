9ec5dfc8ac3ab416077bd3e5537c337732fe0c44
avi-anviam <avirajdhooria2001@gmail.com>
2021-12-12 22:18:53 -0800

    Weather,weatherCard, and DegreeType files written

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import WeatherCard from './WeatherCard.jsx'
+import DegreeType from './DegreeType.jsx'
+import locations from '../data/locations.json';
+
+export default function Weather() {
+    state = {
+        days: [],
+        degreeType: "imperial"
+    }
+    const location = locations;
+    fetchApiData = () => {
+        const weatherUrl = `api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&units=${this.state.degreeType}&appid=0fe2033efdd77a1e5dfb7a392e963afb`;
+        fetch(weatherUrl)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Data List succesfully loaded", data.list)
+                const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+                this.setState({days: dailyData})
+            })
+    }
+
+    // cardFormat = () => {
+    //     return this.state.days.map((day, index) => <WeatherCard day={day} key={index} />)
+    // }
+    const card = state.days.map((day,index) => {
+        return(
+            <WeatherCard day={day} key={index} />
+        )
+    })
+    updateCorF = degreeType => {
+        this.setState({
+            degreeType: degreeType
+        }, this.sendNewData)
+    }
+
+    sendNewData = () => {
+        const weatherUrl = `api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&units=${this.state.degreeType}&appid=0fe2033efdd77a1e5dfb7a392e963afb`;
+        fetch(weatherUrl)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Data List succesfully loaded", data.list)
+                const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+                this.setState({days: dailyData})
+            })
+    }
+
+    return (
+        <div className="container">
+            <h1 className="display-1 jumbotron">5-Day Forecast.</h1>
+            <DegreeType degreeType={this.state.degreeType} updateCorF={this.updateCorF}/>
+            <h5 className="display-5 text-muted">{this.location.name}</h5>
+            <div className="row justify-content-center">
+                  {card}
+            </div>
+        </div>
+      )
+}
---
+++ src/Components/WeatherCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Card.css';
+var moment = require('moment');
+
+export default function WeatherCard(props) {
+    let date = new Date();
+    const dayOfWeek = props.day.dt * 1000
+    date.setTime(dayOfWeek);
+
+    const iconUrl = "owf owf-" + props.day.weather[0].id + " owf-5x red";
+    return (
+        <div className="col-auto">
+        <div className="card">
+          <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
+          <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
+          <i className={iconUrl}></i>
+          <h2>{Math.round(props.day.main.temp)} °F</h2>
+          <div className="card-body">
+            <p className="card-text">{props.day.weather[0].description}</p>
+            <button className="btn btn-dark btn-outline-light">See Hourly Forecast</button>
+          </div>
+        </div>
+      </div>
+    )
+}
