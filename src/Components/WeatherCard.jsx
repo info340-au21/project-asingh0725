a8e77bbc76e03651cc16379b343c4c351e6c0551
avi-anviam <avirajdhooria2001@gmail.com>
2021-12-13 14:13:18 -0800

    something works

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -1,25 +1,63 @@
+// import React from 'react';
+// import '../css/card.css'
+// //var moment = require('moment');
+
+// export default function WeatherCard(props) {
+//     let date = new Date();
+//     const dayOfWeek = props.day.dt * 1000
+//     date.setTime(dayOfWeek);
+
+//     const iconUrl = "owf owf-" + props.day.weather[0].id + " owf-5x red";
+//     return (
+//         <div className="col-auto">
+//         <div className="card">
+//           <h3 className="card-title">{date.format('dddd')}</h3>
+//           <p className="text-muted">{date.format('MMMM Do, h:mm a')}</p>
+//           <i className={iconUrl}></i>
+//           <h2>{Math.round(props.day.main.temp)} °F</h2>
+//           <div className="card-body">
+//             <p className="card-text">{props.day.weather[0].description}</p>
+//             <button className="btn btn-dark btn-outline-light">See Hourly Forecast</button>
+//           </div>
+//         </div>
+//       </div>
+//     )
+// }
+
+
+
 import React from 'react';
-import '../css/card.css'
-//var moment = require('moment');
+import '../css/card.css';
+var moment = require('moment');
 
-export default function WeatherCard(props) {
-    let date = new Date();
-    const dayOfWeek = props.day.dt * 1000
-    date.setTime(dayOfWeek);
+class WeatherCard extends React.Component {
+  // Props: day, key(index)
 
-    const iconUrl = "owf owf-" + props.day.weather[0].id + " owf-5x red";
+  render() {
+    let newDate = new Date();
+    const weekday = this.props.day.dt * 1000
+    newDate.setTime(weekday)
+
+    const imgURL = "owf owf-"+ this.props.day.weather[0].id +" owf-5x red"
+
+    // const farenheit = (parseInt(this.props.day.main.temp) - 273.15) * (9/5) + 32
+
+
     return (
-        <div className="col-auto">
+      <div className="col-auto">
         <div className="card">
-          <h3 className="card-title">{date.format('dddd')}</h3>
-          <p className="text-muted">{date.format('MMMM Do, h:mm a')}</p>
-          <i className={iconUrl}></i>
-          <h2>{Math.round(props.day.main.temp)} °F</h2>
+          <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
+          <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
+          <i className={imgURL}></i>
+          <h2>{Math.round(this.props.day.main.temp)} °F</h2>
           <div className="card-body">
-            <p className="card-text">{props.day.weather[0].description}</p>
+            <p className="card-text">{this.props.day.weather[0].description}</p>
             <button className="btn btn-dark btn-outline-light">See Hourly Forecast</button>
           </div>
         </div>
       </div>
     )
+  }
 }
+
+export default WeatherCard;
