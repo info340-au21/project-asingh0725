7c0bade6f2015f6aa320a4cd112f144812197197
Spencer Pudists <pudists7@uw.edu>
2021-12-13 15:28:05 -0800

    Rentals data and pages incomplete

---
+++ src/Components/Rental.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Rental from './rentalData.json';
+
+function rental (props) {
+    const rental = props.report;
+    return (
+        <div className = ""
+    )
+}
---
+++ src/Components/rentalLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Rental from './rentalData.json';
+
+function Rental(props) {
+    const rental = props.rental
+    return (
+        <div>
+            <DataGrid
+                rows={}
+
+        />
+
+        </div>
+
+
+
+    )
+
+
+}
