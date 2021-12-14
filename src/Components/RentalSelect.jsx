a4ef00975b10e466499b105af4d7a95051b072b0
igz10 <garibayisaac3@gmail.com>
2021-12-13 21:25:36 -0800

    select

--- src/Components/RentalSelect.jsx
+++ src/Components/RentalSelect.jsx
@@ -1,1 +1,24 @@
 import React from 'react';
+
+export default function RentalSelect(props) {
+
+    const chooseActivity = props.map((act, index) => {
+        return <option key={index} value={act}>{act}</option>;
+    })
+
+    return (
+        <div className="react-select-menu">
+            <ul className="react-select-options">
+                <CheckboxOption value="" value={selected} >
+
+                </CheckboxOption>
+            </ul>
+
+        <div className="col-auto">
+            <button id="submitButton" type="submit" className="btn btn-warning" onClick={handleClick} >Apply Filter</button>
+        </div>
+
+        </div>
+        
+    )
+}
