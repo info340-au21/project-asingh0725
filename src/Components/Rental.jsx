01146e8fe003550772583945edcbaf6b4e34474b
Spencer Pudists <pudists7@uw.edu>
2021-12-13 20:06:30 -0800

    Rentals unrouted

--- src/Components/Rental.jsx
+++ src/Components/Rental.jsx
@@ -1,10 +1,27 @@
-/* import React from "react";
-import Rental from './rentalData.json';
+import React from "react";
 
-function rental (props) {
-    const rental = props.report;
+
+export function Header (props) {
+    let colInput = props.columnNames;
+    let content = colInput.map((item) => <th key={item}>{item}</th>)
     return (
-        <div className = ""
-    )
+        <thead>
+            <tr>
+                {content}
+            </tr>
+        </thead>
+    );
 }
-*/
+
+export function RentalRow(props) {
+    let rental = props.rental;
+    return (
+        <tr>
+            <td>{rental.location}</td>
+            <td>{rental.sport}</td>
+            <td>{rental.equipment}</td>
+            <td>{rental.price}</td>
+            <td>{rental.availability}</td>
+        </tr>
+    );
+}
--- src/Components/rentalLayout.jsx
+++ src/Components/rentalLayout.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import Rental from './rentalData.json';
 
-function Rental(props) {
-    const rental = props.rental
-    return (
-        <div>
-            <DataGrid
-                rows={}
-
-        />
-
-        </div>
-
-
-
-    )
-
-
+export function RentalTable (props) {
+    let colHeaders = ["Location", "Sport", "Equipment", "Price", "Availability"];
+    //let rental = props.report;
+    let row = Rental.map((loc) => (<RentalRow key={loc.location} location={loc}/>))
+        return (
+            <table className = 'table table-bordered'>
+                <Header columnNames = {colHeaders} />
+                <tbody>{row}</tbody>
+            </table>
+        )
 }
