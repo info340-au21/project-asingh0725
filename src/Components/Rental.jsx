388081ad2726242ba9c4e6edcd1279aefdb7c953
dgunadharma20 <dgunadharma20@gmail.com>
2021-12-13 20:39:24 -0800

    rental

--- src/Components/Rental.jsx
+++ src/Components/Rental.jsx
@@ -1,27 +1,7 @@
 import React from "react";
 
-
-export function Header (props) {
-    let colInput = props.columnNames;
-    let content = colInput.map((item) => <th key={item}>{item}</th>)
-    return (
-        <thead>
-            <tr>
-                {content}
-            </tr>
-        </thead>
-    );
+function Rental () {
+    
 }
 
-export function RentalRow(props) {
-    let rental = props.rental;
-    return (
-        <tr>
-            <td>{rental.location}</td>
-            <td>{rental.sport}</td>
-            <td>{rental.equipment}</td>
-            <td>{rental.price}</td>
-            <td>{rental.availability}</td>
-        </tr>
-    );
-}
+export default Rental;
